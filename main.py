c23e06d347a850692f694330f5b2a1a55953f116
Juliya-sun <lyubimova.yulya.29@yandex.ru>
2024-08-23 11:25:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 number = int(input('Введите число от 3 до 20: '))
-a = number
 list_ = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
 parol = []
-not_parol = []
-for i in list_:
-    if i == 1:
-        continue
-    is_parol = True
-    for j in range(2, 21):
-        if i // j == 0:
-            is_parol = False
-            break
-    if is_parol:
-        parol.append([i])
-    else:
-        not_parol.append(i)
+# not_parol = []
+for i in range (len(list_)):
+#    if i == 1:
+#        continue
+    for j in range(i+1, len(list_)):
+        if list_[i] + list_[j] == number:
+            parol.append((list_[i], list_[j]))
+#        else:
+#           not_parol.append((list_[i] + list_[j])
 print(parol)
+#print(not_parol)
